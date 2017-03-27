6fc9ae2f4429415aab58e61fb2240246fd0a7ae5
Audi Bayron <z1789895@doits-mac-mini-6.local>
2017-03-27 09:28:34 -0500

    Initial Commit

---
+++ BayronLocalNotification-V2Tests/BayronLocalNotification_V2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  BayronLocalNotification_V2Tests.swift
+//  BayronLocalNotification-V2Tests
+//
+//  Created by Audi Bayron on 3/27/17.
+//  Copyright © 2017 Audi Bayron. All rights reserved.
+//
+
+import XCTest
+@testable import BayronLocalNotification_V2
+
+class BayronLocalNotification_V2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
